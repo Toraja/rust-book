61b5c093f7517e0557050b4e6f02451a45563585
Toraja <toraja.blend.french@gmail.com>
2022-01-04 13:52:34 +0900

    ch3: 3.1 Variables and Mutability

--- ch3/variables/src/main.rs
+++ ch3/variables/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    println!("--- mutablity ---");
+    mutablity();
+    println!("--- shadowing ---");
+    shadowing();
+}
+
+fn mutablity() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+}
+
+fn shadowing() {
+    let x = 5;
+
+    println!("The value of x before shadowing is: {}", x);
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {}", x);
+    }
+
+    println!("The value of x is: {}", x);
 }
