3a93408f7b639d93034e1daac5ae6e6395963b49
Toraja <toraja.blend.french@gmail.com>
2022-01-21 14:35:25 +0900

    ch9: 9.3 To panic! or Not to panic!

--- ch2/guessing_game/src/main.rs
+++ ch2/guessing_game/src/main.rs
@@ -3,6 +3,12 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
+    // guess_original();
+    guess_with_type();
+}
+
+#[allow(dead_code)]
+fn guess_original() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
@@ -30,5 +36,58 @@ fn main() {
                 break;
             }
         }
+    }
+}
+
+#[allow(dead_code)]
+fn guess_with_type() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number!");
+                continue;
+            }
+        };
+        let guess = Guess::new(guess);
+        println!("You guessed: {}", guess.value());
+
+        match guess.value().cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            // It would be more appropriate to return Result since `value` is user input,
+            // but panics here for demonstration.
+            panic!("Guess value must be between 1 and 100, got {}", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
     }
 }
